8e091b714e3e645f20350c75af864347949f199e
Alexandre Bisiaux <bisiaux.alexandre@gmail.com>
2013-01-23 11:14:31 +0100

    dao

--- PolyStunter/src/servlets/AddProductServlet.java
+++ PolyStunter/src/servlets/AddProductServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
+import utils.ErrorMessage;
 import utils.UploadFileOnServer;
 
 import beans.Location;
