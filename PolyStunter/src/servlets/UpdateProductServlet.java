a173cbc3bd271dfb3734caf5d4b1b490a717dae0
Alexandre Bisiaux <bisiaux.alexandre@gmail.com>
2013-01-30 10:07:31 +0100

    updateProduct

--- PolyStunter/src/servlets/UpdateProductServlet.java
+++ PolyStunter/src/servlets/UpdateProductServlet.java
@@ -9,8 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.WarehouseDAO;
+
 import beans.Market;
 import beans.Product;
+import beans.User;
 
 /**
  * Servlet implementation class UpdateProductServlet
@@ -24,11 +27,13 @@ public class UpdateProductServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
+		User user = (User) request.getSession().getAttribute("user");
+		request.setAttribute("warehouses", WarehouseDAO.getInstance().getWarehousesOfSeller(user.getId()));
 		Product p;
 		if((p = Market.getInstance().getProduct(id)) != null)
 		{
 			request.setAttribute("product", p);
-			getServletContext().getRequestDispatcher("/WEB-INF/productUpdate.jsp").forward(request, response);
+			getServletContext().getRequestDispatcher("/WEB-INF/updateProduct.jsp").forward(request, response);
 		} else {
 			getServletContext().getRequestDispatcher("/WEB-INF/store.jsp").forward(request, response);
 		}
